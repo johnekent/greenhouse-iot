0a48b82a843784385468fbca272859a47d8317b9
johnekent <johnekent@gmail.com>
2022-07-06 22:06:43 -0400

    successful melnor and env update

--- sensors/gh.sh
+++ sensors/gh.sh
@@ -1,4 +1,5 @@
-. /home/pi/venvs/iot/bin/activate
+#. /home/pi/venvs/iot/bin/activate
+. /home/pi/venvs/iot39/bin/activate
 #python ~/iot/aws-iot-device-sdk-python-v2/samples/pubsub.py --topic test/chat --root-ca /home/pi/iot/certs/greenhouse-sensor-z2-AmazonRootCA1.pem  --key /home/pi/iot/certs/greenhouse-sensor-z2-private.pem.key --cert /home/pi/iot/certs/greenhouse-sensor-z2-device.pem.crt --endpoint a1n6fj3nzgfi04-ats.iot.us-east-1.amazonaws.com
 ROOT_CA=/home/pi/iot/certs/greenhouse-sensor-z2-AmazonRootCA1.pem
 KEY=/home/pi/iot/certs/greenhouse-sensor-z2-private.pem.key
--- test/melnorbt.py
+++ test/melnorbt.py
@@ -1,17 +1,46 @@
 import asyncio
+import time
 
 from melnor_bluetooth.constants import BATTERY_UUID
 from melnor_bluetooth.device import Device
 
 #address = '00:00:00:00:00' # fill with your device mac address
-address = '58:93:D8:AC:81:26' # fill with your device mac address
+address = '58:93:D8:AC:81:26' # this is the melnor greenhouse address
+#address = '59:93:D8:AC:81:26' # this is the wrong address -- does not connect when this is used
 
 async def test():
     device = Device(mac=address)
-    print(f"Device = {device}")
+    print(f"Device before connect = {device}")
     await device.connect()
+    print(f"Device Connected = {device}")
+    await device.fetch_state();
+    print(f"Device after fetching state = {device}")
 
-    print(device.battery_level())
+    print(device.battery_level)
+
+    device.zone1.is_watering = True;
+    await device.push_state();
+    print(f"Device After Zone 1 Watering Set = {device}")
+
+    manual_minutes = 2
+    device.zone1.manual_watering_minutes = manual_minutes
+    device.zone2.manual_watering_minutes = manual_minutes
+    device.zone3.manual_watering_minutes = manual_minutes
+    device.zone4.manual_watering_minutes = manual_minutes
+    await device.push_state();
+    print(f"Device After Manual Minutes = {device}")
+
+
+    start_seconds = time.time()
+    to_sleep = 10
+    remain = manual_minutes * 60 + 30
+    while remain > 0:
+        remain -= to_sleep
+        time.sleep(to_sleep)
+        await device.fetch_state();
+        now = time.time()
+        print(f"Device with {now - start_seconds} elapsed fetching state = {device}")
+        print(f"Zone 1 Watering end time (aka seconds_left) minus now is {device.zone1.watering_end_time - now}")
 
     await device.disconnect();
 
