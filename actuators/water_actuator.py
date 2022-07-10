cfa3fa2a8d7d7e2c35696609e692ce5cb1535434
johnekent <johnekent@gmail.com>
2022-07-10 18:48:43 -0400

    water actuator refactor

--- actuators/water_actuator.py
+++ actuators/water_actuator.py
@@ -1,3 +1,7 @@
+""" water_actuator.py
+
+Simple wrapper around melnor_bluetooth for a subset of relevant functionality to this project.
+"""
 import asyncio
 import logging
 import time
@@ -6,21 +10,38 @@ from melnor_bluetooth.constants import BATTERY_UUID
 from melnor_bluetooth.device import Device
 
 class WaterActuator:
-
+    """Perform functions such as manual watering based on commands.
+    Rather than getting all fancy with asyncio, including event loop inconsistencies between windows
+    and linux, this keeps it simple by connecting, acting, and disconnecting each time.
+    Also, since these commands will be infrequent and connections can fail between, it just makes sense.
+    """
     def __init__(self):
-        address = '58:93:D8:AC:81:26' # this is the melnor greenhouse address
-        self.device = Device(mac=address)
-        asyncio.run(self.__connect__())
-        logging.info(f"Device Connected = {self.device}")
+        self.address = '58:93:D8:AC:81:26' # this is the melnor greenhouse address
+        try:
+            # check and use this to instantiate to prevent calling methods on an inaccessible connection
+            asyncio.run(self.validate_connection(self.address))
+        except Exception as e:
+            logging.critical(f"Failed to successfully connect to device on address {self.address}")
 
-    async def __connect__(self):
-        """Connect to the bluetooth device and fetch the current state
+    async def validate_connection(self):
+        """Make sure the connection can be established and is good through a basic test.
+
+        Args:
+            address (str): the mac address address of the watering device
         """
-        await self.device.connect()   
-        await self.device.fetch_state()
 
-    def zone_map(self, zone: int):
-        """_summary_
+        device = Device(mac=self.address)
+        await device.connect()
+        await device.fetch_state()
+        logging.info(f"Device = {device}")
+
+        await device.disconnect()
+        logging.info(f"Device disconnected")
+        ## let any exceptions float up
+
+    @staticmethod
+    def zone_map(device, zone: int):
+        """Map the id to the zone on the device.
 
         Args:
             zone (int): Integer form of the zone
@@ -30,50 +51,53 @@ class WaterActuator:
         """
 
         if zone == 1:
-            return self.device.zone1
+            return device.zone1
         elif zone == 2:
-            return self.device.zone2
+            return device.zone2
         elif zone == 3:
-            return self.device.zone3
+            return device.zone3
         elif zone == 4:
-            return self.device.zone4
+            return device.zone4
         else:
             raise ValueError(f"The provided zone was {zone} but must be in range between 1 and 4 inclusive.")
 
-    
-    def water_zone(self, zone: int, minutes: int):
+    async def water_zone(self, zone: int, minutes: int):
         """Water the specified zone
 
         Args:
             zone (int): The integer form of the zone (1,2,3 or 4)
             minutes (int): Duration of watering
         """
+        device = Device(mac=self.address)
+        device_zone = self.zone_map(device, zone)
 
-        device_zone = self.zone_map(zone)
+        await device.connect()
+        await device.fetch_state()
+        logging.info(f"Device before setting watering zone {zone} to minutes {minutes} = {device}")
 
-        asyncio.run(self.__water_zone__(device_zone, minutes))
-
-    async def __water_zone__(self, zone, minutes):
-        """ Begin watering the specific zone for the specific interval
-
-        Args:
-            zone (melnor_bluetooth.device.zone) : The zone to water
-            minutes (int): The duration of the watering
-        """
         zone.manual_watering_minutes = minutes
         zone.is_watering = True
-        await self.device.push_state()
-        await self.device.fetch_state()
-        print(f"Device After Zone Watering Set = {self.device}")
+        await device.push_state()
+        await device.fetch_state()
+        print(f"Device After Zone Watering Set = {device}")
+        await device.disconnect()
 
-    def disconnect(self):
-        asyncio.run(self.__disconnect__())
+    async def check_battery(self):
 
-    async def __disconnect__(self):
-        await self.device.disconnect()
+        device = Device(mac=self.address)
 
+        await device.connect()
+        await device.fetch_state()
+        logging.info(f"Device in battery check = {device}")
+
+        battery = device.battery_level
+        await device.disconnect()
+
+        return battery    
 
 if __name__ == "__main__":
     wa = WaterActuator()
-    wa.water_zone(zone=1, minutes=1)
-    wa.disconnect()
+    asyncio.run(wa.water_zone(zone=1, minutes=1))
+
+    battery = asyncio.run(wa.check_battery)
+    print("battery is {battery}")
