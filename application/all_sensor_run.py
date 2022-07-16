4c4b1705d096994dc4a465b88359bed3dc376905
johnekent <johnekent@gmail.com>
2022-07-16 18:30:26 -0400

    tested float and fixed connection value

--- application/all_sensor_run.py
+++ application/all_sensor_run.py
@@ -2,6 +2,7 @@
 Does not work in builds or on windows development machine.
 Run through test of known sensors.
 """
+import logging
 
 from app.sensor import TempHumiditySensor
 from app.sensor import TempHumiditySensorI2C
@@ -12,6 +13,7 @@ from app.sensor import FloatSwitchSensor
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
     sensors = [TempHumiditySensor(), TempHumiditySensorI2C(), LightSensor(), LightSensorUV(), WaterProbe(), FloatSwitchSensor()]
 
---
+++ sensors/__init__.py
@@ -0,0 +1,6 @@
+from . import temp_humidity_sensor 
+from temp_humidity_sensor import TempHumiditySensor
+from .temp_humidity_sensor_i2c import TempHumiditySensorI2C
+from .light_sensor import LightSensor
+from .light_sensor_uv import LightSensorUV
+from .water_probe import WaterProbe
