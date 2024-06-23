fc2b6851796f0182d664da02f09f91973698932c
johnekent <johnekent@gmail.com>
2024-06-23 12:47:25 -0400

    log directory

--- application/gh.sh
+++ application/gh.sh
@@ -1,3 +1,3 @@
 #. /home/pi/venvs/iot/bin/activate
 . /home/pi/venvs/iot39/bin/activate
-nohup python ./driver.py --topic greenhouse/metrics | tee output.log &
+nohup python ./driver.py --topic greenhouse/metrics | tee /home/pi/iot/logs/output.log &
